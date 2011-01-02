fc172f27b8a4ecc4627e069fc3608af87d249229
Luis Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2011-01-02 18:16:10 -0200

    algoritimo de calculo de numeros primos

---
+++ primo.php
@@ -0,0 +1,88 @@
+<?php
+set_time_limit(6000);
+
+session_start();
+//session_destroy();
+
+echo("<pre>");
+$limite_media = 5;
+
+$tini = getmicrotime();
+$max = 10000000;
+$primo = array();
+$q = true;
+$atual = 1;
+for ($i = 3; $i <= $max; $i = $i+2) {
+	
+	if($q){
+		$mult = $i*$i;
+
+		if($mult < $max){
+			$quad[$mult] = $i;
+		} else {
+			$q = false;
+		}
+	}
+	
+	if(isset($quad[$i])){
+		$atual = $quad[$i];
+		continue;
+	}
+
+	$a = false;
+	foreach ($primo as $p){
+
+		if($p > $atual){
+			break;
+		}
+
+		if ($i%$p==0){
+			$a = true;
+			break;
+		}
+
+	}
+	
+	if(!$a){
+		$primo[] = $i;
+	}
+
+}
+array_unshift($primo, 2);
+$fini = getmicrotime();
+
+print_r("\n");
+$tempo = $fini - $tini;
+$_SESSION['time'][] = $tempo;
+if(sizeof($_SESSION['time']) > $limite_media)
+{
+	array_shift(&$_SESSION['time']);
+}
+echo("tempo: ".getmedia()."\n");
+
+print_r(sizeof($primo)."/$max");
+print_r("\n");
+print_r($_SESSION['time']);
+print_r("\n");
+print_r($primo);
+print_r("\n");
+print_r($quad);
+
+
+function getmicrotime()
+{
+	list($usec, $sec) = explode(" ", microtime());
+	return ((float)$usec + (float)$sec);
+}
+
+function getmedia() {
+	$soma = 0;
+	$i = 0;
+	foreach ($_SESSION['time'] as $value) {
+		$soma += $value;
+		$i++;
+	}
+	return $soma/$i;
+}
+
+?>
